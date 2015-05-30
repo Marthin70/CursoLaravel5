d67f4f29e1d115091c0d4fa451ef43e25ef017fb
Raul Palacios <palacios.raul02@gmail.com>
2015-05-30 15:39:50 -0500

    Proyecto con nuestras primeras vistas

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -11,52 +11,6 @@
 | post,get,put,delete
 */
 
-Route::get('controlador','PruebaController@index');
-Route::get('name/{nombre}','PruebaController@nombre');
-
-
-Route::resource('movie','MovieController');
-
-
-/*
-| Ruta Basica
-*/
-Route::get('prueba', function(){
-	return "Hola desde routes.php";
-});
-
-/*
-| Ruta Basica Con parametro
-*/
-/*
-| Estructura
-| Route:: -> Referencia a clase
-| get -> Metodo HTTP
-| 'nombre' -> Nombre de la URL en el navegador
-| '{nombre}' -> parametro que recibiremos mediante la ruta
-| function($nombre) -> funcion recibiendo el parametro
-*/
-Route::get('nombre/{nombre}', function($nombre){
-	return "Mi nombre es: ".$nombre;
-});
-
-Route::get('edad/{edad}', function($edad){
-	return "Mi edad es: ".$edad;
-});
-
-/*
-| Ruta Basica Con parametro opcional
-*/
-Route::get('edad2/{edad?}', function($edad = 20){
-	return "Mi edad es: ".$edad;
-});
-
-
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::get('/','FrontController@index');
+Route::get('contacto','FrontController@contacto');
+Route::get('reviews','FrontController@reviews');
---
+++ laravel/resources/views/reviews.php
@@ -0,0 +1,131 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cinema A Entertainment Category Flat Bootstarp Resposive Website Template | Reviews :: w3layouts</title>
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- Custom Theme files -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Cinema Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<!-- header-section-starts -->
+	<div class="full">
+			<div class="menu">
+				<ul>
+					<li><a href="index.html"><div class="hm"><i class="home1"></i><i class="home2"></i></div></a></li>
+					<li><a class="active" href="reviews.html"><div class="cat"><i class="watching"></i><i class="watching1"></i></div></a></li>
+					<li><a href="contact.html"><div class="cnt"><i class="contact"></i><i class="contact1"></i></div></a></li>
+				</ul>
+			</div>
+		<div class="main">
+		<div class="review-content">
+			<div class="top-header span_top">
+				<div class="logo">
+					<a href="index.html"><img src="images/logo.png" alt="" /></a>
+					<p>Movie Theater</p>
+				</div>
+
+				<div class="clearfix"></div>
+			</div>
+			<div class="reviews-section">
+				<h3 class="head">Movie Reviews</h3>
+					<div class="col-md-9 reviews-grids">
+						<div class="review">
+							<div class="movie-pic">
+								<img src="images/r4.jpg" alt="" />
+							</div>
+							<div class="review-info">
+								<a class="span" href="single.html">Lorem  <i>Movie Review</i></a>
+								<p class="dirctr"><a href="">Reagan Gavin Rasquinha, </a>TNN, Mar 12, 2015, 12.47PM IST</p>								
+								<p class="info">CAST:&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Will Smith, Margot Robbie, Adrian Martinez, Rodrigo Santoro, BD Wong, Robert Taylor</p>
+								<p class="info">DIRECTION: &nbsp;&nbsp;&nbsp;&nbsp;Glenn Ficarra, John Requa</p>
+								<p class="info">GENRE:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Crime</p>
+								<p class="info">DURATION:&nbsp;&nbsp;&nbsp; &nbsp; 1 hour 45 minutes</p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+
+						<div class="review">
+							<div class="movie-pic">
+								<img src="images/r6.jpg" alt="" />
+							</div>
+							<div class="review-info">
+								<a class="span" href="single.html">Lorem  <i>Movie Review</i></a>
+								<p class="dirctr"><a href="">Reagan Gavin Rasquinha, </a>TNN, Mar 12, 2015, 12.47PM IST</p>
+								<div class="clearfix"></div>
+								<p class="info">CAST:&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Will Smith, Margot Robbie, Adrian Martinez, Rodrigo Santoro, BD Wong, Robert Taylor</p>
+								<p class="info">DIRECTION: &nbsp;&nbsp;&nbsp;&nbsp;Glenn Ficarra, John Requa</p>
+								<p class="info">GENRE:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Crime</p>
+								<p class="info">DURATION:&nbsp;&nbsp;&nbsp; &nbsp; 1 hour 45 minutes</p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+		
+
+					</div>
+				<div class="clearfix"></div>
+			</div>
+			</div>
+		<div class="review-slider">
+			 <ul id="flexiselDemo1">
+			<li><img src="images/r1.jpg" alt=""/></li>
+			<li><img src="images/r2.jpg" alt=""/></li>
+			<li><img src="images/r3.jpg" alt=""/></li>
+			<li><img src="images/r4.jpg" alt=""/></li>
+			<li><img src="images/r5.jpg" alt=""/></li>
+			<li><img src="images/r6.jpg" alt=""/></li>
+		</ul>
+			<script type="text/javascript">
+		$(window).load(function() {
+			
+		  $("#flexiselDemo1").flexisel({
+				visibleItems: 6,
+				animationSpeed: 1000,
+				autoPlay: true,
+				autoPlaySpeed: 3000,    		
+				pauseOnHover: false,
+				enableResponsiveBreakpoints: true,
+				responsiveBreakpoints: { 
+					portrait: { 
+						changePoint:480,
+						visibleItems: 2
+					}, 
+					landscape: { 
+						changePoint:640,
+						visibleItems: 3
+					},
+					tablet: { 
+						changePoint:768,
+						visibleItems: 3
+					}
+				}
+			});
+			});
+		</script>
+		<script type="text/javascript" src="js/jquery.flexisel.js"></script>	
+		</div>		
+	<div class="footer">
+		<h6>Disclaimer : </h6>
+		<p class="claim">This is a freebies and not an official website, I have no intention of disclose any movie, brand, news.My goal here is to train or excercise my skill and share this freebies.</p>
+		<a href="example@mail.com">example@mail.com</a>
+		<div class="copyright">
+			<p> Template by  <a href="http://w3layouts.com">  W3layouts</a></p>
+		</div>
+	</div>	
+	</div>
+	<div class="clearfix"></div>
+	</div>
+</body>
+</html>
